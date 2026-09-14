6e3eacc1ff1d6bb07d5fb2a962f8526f90755d27
Matsiko Keneth <matsken256@gmail.com>
2026-09-14 08:41:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,23 +1,8 @@
+
 module.exports = {
-  // BOT INFO
   botName: "BREAKER-ULTRA-MD",
   ownerName: "Matsken",
-  ownerNumber: "256747701543",
+  ownerNumber: "256747701543", // <-- NEVER CHANGE! This is the creator/owner
+  phoneNumber: "256747701543", // <-- USER CHANGE THIS! Put your bot number here
   prefix: ".",
-  
-  // VERSION
-  version: "1.0.0",
-  
-  // YOUR LINKS
-  github: "https://github.com/matsken256-boop/BREAKER-ULTRA-MD",
-  youtube: "https://youtube.com/@matsken",
-  
-  // MESSAGES
-  mess: {
-    owner: "This command is only for my owner Matsken!",
-    group: "This is for groups only!",
-    private: "This is for private chat only!",
-    admin: "You need to be admin!",
-    botAdmin: "Make me admin first!"
-  }
 }
