6df25bb5bf8c25046cddcea32a59edb1307bc01e
Matsiko Keneth <matsken256@gmail.com>
2026-09-14 07:00:45 +0300

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // BOT INFO
+  botName: "BREAKER-ULTRA-MD",
+  ownerName: "Matsken",
+  ownerNumber: "256747701543",
+  prefix: ".",
+  
+  // VERSION
+  version: "1.0.0",
+  
+  // YOUR LINKS
+  github: "https://github.com/matsken256-boop/BREAKER-ULTRA-MD",
+  youtube: "https://youtube.com/@matsken",
+  
+  // MESSAGES
+  mess: {
+    owner: "This command is only for my owner Matsken!",
+    group: "This is for groups only!",
+    private: "This is for private chat only!",
+    admin: "You need to be admin!",
+    botAdmin: "Make me admin first!"
+  }
+}
